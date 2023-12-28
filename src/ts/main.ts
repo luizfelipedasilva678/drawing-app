cbe5146ccea4630e2fcd5f55739f682c6a0fe2f2
Luiz Felipe <luizfelipedasilva678@gmail.com>
2023-12-28 16:28:52 -0300

    Initiating canvas

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,17 @@
 import "../css/style.css";
+
+const canvas = document.getElementById("drawing-area") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
+if (ctx) {
+  ctx.beginPath();
+  ctx.fillStyle = "red";
+  ctx.fillRect(0, 0, 300, 300);
+  ctx.fill();
+} else {
+  alert("Canvas not supported");
+}
